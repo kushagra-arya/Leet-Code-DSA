693fae6e60f9c98279c577889aa3d6b6cb02ed88
Kushagra <kushagraarya1801@gmail.com>
2025-08-15 22:25:28 +0530

    Time: 19 ms (45.12%) | Memory: 15.5 MB (6.13%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        s = ''.join(ch.lower() for ch in s if ch.isalnum())
+
+        k = 0
+        if len(s) == 0:
+            return True
+        for i in range(len(s) - 1, -1, -1):
+            if s[k] != s[i]:
+                return False
+            k += 1
+        return True
