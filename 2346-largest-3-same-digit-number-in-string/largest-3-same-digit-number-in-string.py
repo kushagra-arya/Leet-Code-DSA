428e1a5f38194f2d6a4da2ce57272c5ecc85c03c
Kushagra <kushagraarya1801@gmail.com>
2025-08-16 21:45:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (21.82%) - LeetSync

---
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def largestGoodInteger(self, num):
+        lst = ["999","888","777","666","555","444","333","222","111","000"]
+        for i in range(len(lst)):
+            if lst[i] in num:
+                return lst[i]
+        else:
+            return ""
+            
